6722dc4821ea47df6f09890ddbc3f48ee39d0517
krisograbek <krzysztof.ograbek@gmail.com>
2021-10-29 13:22:09 +0200

    install mui/styles, add Grid

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import IconButton from '@mui/material/IconButton';
-import Button from '@mui/material/Button';
+import Brightness3Outlined from '@mui/icons-material/Brightness3Outlined';
+import WbSunnyOutlined from '@mui/icons-material/WbSunnyOutlined';
 import { grey } from '@mui/material/colors';
+import IconButton from '@mui/material/IconButton';
 import Paper from '@mui/material/Paper';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { makeStyles } from '@mui/styles';
 import Typography from '@mui/material/Typography';
 import { useState } from 'react';
 import './App.css';
-import Brightness3Outlined from '@mui/icons-material/Brightness3Outlined';
-import WbSunnyOutlined from '@mui/icons-material/WbSunnyOutlined';
+import { Container, Grid } from '@mui/material';
 
 
 const lightTheme = createTheme({
@@ -51,38 +52,55 @@ const darkTheme = createTheme({
   }
 });
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    minHeight: '100vh',
+    padding: 20
+  }
+}));
+
 function App() {
+  const classes = useStyles();
   const [themeMode, setThemeMode] = useState(true);
   const theme = themeMode ? darkTheme : lightTheme;
-  const icon = themeMode ? <WbSunnyOutlined /> : <Brightness3Outlined />;
+  const Icon = themeMode ? WbSunnyOutlined : Brightness3Outlined;
 
   return (
     <ThemeProvider theme={theme}>
-      <Paper elevation={0} square={true}>
-        <header className="App-header">
-          <Typography variant="h4">
-            Hi, I am an important information
-          </Typography>
-          <Typography variant="subtitle1">
-            I am a body text. You'll see me all over the place
-          </Typography>
-          <Typography variant="subtitle1" color="text.secondary">
-            I am also important
-          </Typography>
-          <Typography variant="subtitle1" color="primary">
-            You can click me
-          </Typography>
-          <IconButton
-            edge="end"
-            color="inherit"
-            aria-label="mode"
-            onClick={() => setThemeMode(!themeMode)}
+      <Container>
+        <Paper className={classes.root} elevation={0} square={true}>
+          <Grid
+            container
+            direction="column"
           >
-            {icon}
-          </IconButton>
-          <Button onClick={() => setThemeMode(!themeMode)} color="primary" variant="outlined">Click me</Button>
-        </header>
-      </Paper>
+            <Grid item>
+              <Typography variant="h4">
+                Hi, I am an important information
+              </Typography>
+              <Typography variant="subtitle1">
+                I am a body text. You'll see me all over the place
+              </Typography>
+              <Typography variant="subtitle1" color="text.secondary">
+                I am also important
+              </Typography>
+              <Typography variant="subtitle1" color="primary">
+                You can click me
+              </Typography>
+            </Grid>
+            <Grid item>
+              <IconButton
+                edge="end"
+                color="primary"
+                size="small"
+                aria-label="mode"
+                onClick={() => setThemeMode(!themeMode)}
+              >
+                <Icon fontSize="large" />
+              </IconButton>
+            </Grid>
+          </Grid>
+        </Paper>
+      </Container>
     </ThemeProvider>
   );
 }
